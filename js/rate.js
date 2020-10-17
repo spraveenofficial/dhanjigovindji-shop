2dae2014186a67e2c170fa7f2fb0d184a2365fae
unknown <spraveen593@gmail.com>
2020-10-17 12:33:14 +0530

    Edited

--- js/rate.js
+++ js/rate.js
@@ -1,16 +1,31 @@
-fetch('https://test.1ounce.in/')
-    .then(res => res.json())
-    .then((rates) => {
-        flactuatedRateInGold22 = 1114;
-        flactuatedRateInGold24 = 2758;
-        gold22data = rates.gold22*10;
-        gold24data = rates.gold24*10;
-        silver = rates.silver*1000;
-        price = gold22data.toFixed(0).toLocaleString("en-IN");
-        price1 = gold24data.toFixed(0).toLocaleString("en-IN");
-        price2 = silver.toFixed(2).toLocaleString("en-IN");
-        // console.log(price, price1, price2);
-        $("#rates").text("Rs."+ price)
-        $("#rates1").text("Rs."+ price1)
-        $("#rates2").text("Rs."+ price2)
-})
+// // fetch('https://test.1ounce.in/')
+// //     .then(res => res.json())
+// //     .then((rates) => {
+// //         flactuatedRateInGold22 = 1114;
+// //         flactuatedRateInGold24 = 2758;
+// //         gold22data = rates.gold22*10;
+// //         gold24data = rates.gold24*10;
+// //         silver = rates.silver*1000;
+// //         price = gold22data.toFixed(0).toLocaleString("en-IN");
+// //         price1 = gold24data.toFixed(0).toLocaleString("en-IN");
+// //         price2 = silver.toFixed(2).toLocaleString("en-IN");
+// //         console.log(price, price1, price2);
+// //         // console.log(rates);
+// //         $("#rates").text("Rs."+ price)
+// //         $("#rates1").text("Rs."+ price1)
+// //         $("#rates2").text("Rs."+ price2)
+// // })
+
+// fetch("https://rapidapi.p.rapidapi.com/pokemon_stats.json", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "pokemon-go1.p.rapidapi.com",
+// 		"x-rapidapi-key": "c2faf13e7fmsh88eb4200d663ebep176572jsndbeb592357ae"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
---
+++ js/rates.js
@@ -0,0 +1,22 @@
+var cors = ('http://cors-anywhere.herokuapp.com/')
+fetch( cors + 'http://ritikakeel.com/vinny/vinnytam')
+.then(response => response.json())
+.then(data => {
+  var bread_difference = 250;
+  console.log(data);
+  rateData = data.Bhaw;
+  firstData = data.Bhaw[0].Gold_RED;
+  secondData = data.Bhaw[0].Gold_RED - bread_difference;
+  thirdData = data.Bhaw[0].Silver;
+  lastUpdated = data.Bhaw[0].TimeGold;
+  console.log(firstData)
+  console.log(secondData)
+  console.log(thirdData)
+  $("#rates").text("Rs."+ firstData)
+  $("#rates1").text("Rs."+ secondData)
+  $("#rates2").text("Rs."+ thirdData)
+  $("#lastupdated").text("Last Updated : "+ lastUpdated)
+})
+.catch(error => {
+console.log("Error While Getting Data")
+})
